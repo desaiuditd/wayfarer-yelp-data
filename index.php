34d6e214776dea727f2f8e087e92c158cb4c7dda
Manoj <mparihar@scu.edu>
2016-10-15 14:22:09 -0500

    Adds Twitter oAuth and GET response from Twitter.

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
  */
 
 include_once "../ee-config.php";
-
+require "twitteroauth/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+define ('CONSUMER_KEY', "w3Bsm5zzNf9GBsZ6aO7zxGWhC");
+define ('CONSUMER_SECRET', "YNakI4IOV3tiRNlBbxrpuXWaYVhyNOPHOVxJM2zD3VRARm4UjK");
 $twitter_handle = $_GET['twitter_handle'];
 $city = $_GET['city'];
 $city = '%'.$city.'%';
@@ -23,6 +26,13 @@ if ( empty($twitter_handle) || empty($city) ) {
 }
 
 // 1. Take twitter handle & fetch tweets
+$access_token = "2440917456-2mJtKDy6LJEvANfsQIR3KxdU3udyNHKxqZR8HAQ";
+$access_token_secret = "TL8acUKIJXaUPEACs2Tei2XxFcCJLI70VzosCsZTqwBch";
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token, $access_token_secret);
+$content = $connection->get("account/verify_credentials");
+
+$response = $connection->get("search/tweets", ["q" => $twitter_handle]);
+var_dump($response);
 
 // 2. Take city & fetch reviews
 
