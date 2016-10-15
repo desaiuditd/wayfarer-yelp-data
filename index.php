bc559b41a3cedfd3e1ede2abb523189af8eb9ddc
Udit Desai <desaiuditd@gmail.com>
2016-10-15 09:39:29 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: udit
+ * Date: 15/10/16
+ * Time: 09:04
+ */
+
+include_once "../ee-config.php";
+
+// Create connection
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+// Check connection
+if ($conn->connect_error) {
+	header('Content-Type: application/json');
+	$json = array(
+		'error_code' => 500,
+		'message' => 'Connection failed: ' . $conn->connect_error,
+	);
+	echo json_encode($json);
+	exit();
+}
+
+$sql = "SELECT * FROM review LIMIT = 10";
+$result = $conn->query($sql);
+
+$reviews = array();
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		$reviews[] = $row;
+	}
+}
+$conn->close();
+
+header('Content-Type: application/json');
+echo json_encode($reviews);
