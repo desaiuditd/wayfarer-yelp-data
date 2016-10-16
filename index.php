6f5d5bcc47a4e47033998d08ec77241f38cf085a
Udit Desai <desaiuditd@gmail.com>
2016-10-16 00:53:17 -0500

    adds debug

--- index.php
+++ index.php
@@ -178,7 +178,7 @@ if (! empty($twitter_handle)) {
 $twitter_pi     = get_personal_insights($textForPI);
 error_log(var_export($twitter_pi,true));
 $twitter_scores = array();
-if ($twitter_pi && $twitter_pi->tree && $twitter_pi->tree && $twitter_pi->tree->children) {
+if (!empty($twitter_pi) && !empty($twitter_pi->tree) && !empty($twitter_pi->tree->children)) {
 	$twitter_scores[ 'personality'][ 'Openness'] = $twitter_pi->tree->children[ 0]->children[ 0]->children[ 0]->percentage;
 	$twitter_scores[ 'personality'][ 'Conscientiousness'] = $twitter_pi->tree->children[ 0]->children[ 0]->children[ 1]->percentage;
 	$twitter_scores[ 'personality'][ 'Extraversion'] = $twitter_pi->tree->children[ 0]->children[ 0]->children[ 1]->percentage;
